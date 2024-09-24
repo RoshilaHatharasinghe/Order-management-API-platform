43f62ce19b195b0da1c7d85460759583866f3a78
Roshila Hatharasinghe <roshila@liveroom.dev>
2024-09-24 10:07:31 +0530

    implemented APIs for user SignUp and Login, generate token for authenticated users

---
+++ API_platform/src/main/java/com/APIPlatform/API_platform/DTO/UserLoginDTO.java
@@ -0,0 +1,16 @@
+package com.APIPlatform.API_platform.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UserLoginDTO {
+    private String email;
+    private String password;
+
+    public UserLoginDTO(String email, String password) {}
+
+    public UserLoginDTO() {}
+
+}
--- API_platform/src/main/java/com/APIPlatform/API_platform/Service/UserService.java
+++ API_platform/src/main/java/com/APIPlatform/API_platform/Service/UserService.java
@@ -1,12 +1,49 @@
 package com.APIPlatform.API_platform.Service;
 
 import com.APIPlatform.API_platform.DTO.UserDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import com.APIPlatform.API_platform.DTO.UserLoginDTO;
+import com.APIPlatform.API_platform.Entity.User;
+import com.APIPlatform.API_platform.Repository.UserRepo;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface UserService {
+public class UserService {
 
-    public String addUser(UserDTO userDTO);
+    private final UserRepo userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public UserService(UserRepo userRepository, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder)
+    {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User addUser(UserDTO userDTO) {
+        User user = new User(
+                userDTO.getUser_id(),
+                userDTO.getFirst_name(),
+                userDTO.getLast_name(),
+                userDTO.getEmail(),
+                this.passwordEncoder.encode(userDTO.getPassword())
+
+        );
+        return userRepository.save(user);
+    }
+
+    public User loginUser(UserLoginDTO userLoginDTO) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        userLoginDTO.getEmail(),
+                        userLoginDTO.getPassword()
+                )
+        );
+
+        return userRepository.findByEmail(userLoginDTO.getEmail()).orElseThrow();
+    }
 }
